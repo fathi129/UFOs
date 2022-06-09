7939624a7c6a472e3a73c319d89dffd3a7059544
fathi129 <nasreen.fathi@gmail.com>
2022-06-09 01:22:51 -0500

    Challenge module added

---
+++ Practice_Script/interviewprep.js
@@ -0,0 +1,24 @@
+// var arr = [1,2,3,4,5]
+// var n = 3;
+
+// // Array rotation
+// // Given the array arr,we have to rotate n times here n= 2
+// ans = rotationLeft(arr,n);
+// console.log(ans)
+// function rotationLeft(arr,n){
+//     var temp = arr;
+// for(let i=0;i<n;i++){
+//     var first = temp.shift();
+//     temp.push(first);
+// }
+// return temp;
+// }
+
+// rotationLeft = (arr,n) => {
+
+// }
+
+
+var str = "This_is_string";
+var sam = str.split('_')
+console.log(sam);
